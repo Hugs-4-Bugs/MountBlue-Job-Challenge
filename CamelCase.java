5bfe943d27cf689daa338ed088bea72d202616ad
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-01-15 17:16:11 +0530

    Update CamelCase.java

--- CamelCase.java
+++ CamelCase.java
@@ -1,12 +1,9 @@
-public static int camelcase(String s) {
-    // Write your code here
-        int cnt=1;
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
-            if(ch>='A'&&ch<='Z')
-                cnt++;
-        }
-        return cnt;
-
+public static int viralAdvertising(int n) {
+    int liked = 0, cumulative = 0, shared = 5;
+    for (int i = 0; i < n; i++) {
+        liked = shared / 2;
+        cumulative += liked;
+        shared = liked * 3;
     }
+    return cumulative;
+}
